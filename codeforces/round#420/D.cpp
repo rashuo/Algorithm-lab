c4ec7331e0d646d3c3413b1f356aa7153de08028
rashuo <rashuorz@gmail.com>
2017-06-29 22:30:53 +0800

    add round#420#D

--- codeforces/round#420/D.cpp
+++ codeforces/round#420/D.cpp
@@ -4,7 +4,7 @@
 #include <cstring>
 #include <algorithm>
 using namespace std;
-const bool debug = true;
+const bool debug = false;
 const int maxn = 1e4 + 10;
 const int MOD = 1000000007;
 vector<int> vx[maxn], vy[maxn];
@@ -23,26 +23,34 @@ int mabs(int a) {
     return a > 0 ? a : -a;
 }
 void itex(int xx, int yy, int id) {
-    for (int idx : vx[xx]) if (!vis[idx]) {
-        if (mabs(y[idx]-yy)==1) {
-            d[idx] = min(d[idx],d[id]);
-            Q.push(Node(idx,d[idx]));
+    for (int idx : vx[xx]) if (id!=idx && !vis[idx]) {
+        if (mabs(y[idx]-yy)==1 && x[id]==x[idx]) {
+            if (d[idx] > d[id]) {
+                d[idx] = d[id]; 
+                Q.push(Node(idx,d[idx]));
+            }
         }
         else {
-            d[idx] = min(d[idx],d[id]+1);
-            Q.push(Node(idx,d[idx]));
+            if (d[idx] > d[id]+1) {
+                d[idx] = d[id]+1; 
+                Q.push(Node(idx,d[idx]));
+            }
         }
     }
 }
 void itey(int xx, int yy, int id) {
-    for (int idy : vy[yy]) if (!vis[idy]) {
-        if (mabs(x[idy]-xx)==1) {
-            d[idy] = min(d[idy],d[id]);
-            Q.push(Node(idy,d[idy]));
+    for (int idy : vy[yy]) if (id!=idy && !vis[idy]) {
+        if (mabs(x[idy]-xx)==1 && y[id]==y[idy]) {
+            if (d[idy] > d[id]) {
+                d[idy] = d[id]; 
+                Q.push(Node(idy,d[idy]));
+            }
         }
         else {
-            d[idy] = min(d[idy],d[id]+1);
-            Q.push(Node(idy,d[idy]));
+            if (d[idy] > d[id]+1) {
+                d[idy] = d[id]+1; 
+                Q.push(Node(idy,d[idy]));
+            }
         }
     }
 }
@@ -58,7 +66,7 @@ int main() {
         vx[x[i]].push_back(i);
         vy[y[i]].push_back(i);
     }
-
+    d[st] = 0;
     Q.push(Node(st,0));
 
     while(!Q.empty()) {
@@ -68,14 +76,18 @@ int main() {
 
         int xx = x[tp.id], yy = y[tp.id], v = tp.v;
         itex(xx, yy, tp.id);
-        if (xx>0) itex(xx-1,yy,tp.id);
+        if (xx>2) itex(xx-2,yy,tp.id);
+        if (xx>1) itex(xx-1,yy,tp.id);
         itex(xx+1,yy,tp.id);
+        itex(xx+2,yy,tp.id);
 
         itey(xx, yy, tp.id);
-        if (yy>0) itey(xx,yy-1,tp.id);
+        if (yy>1) itey(xx,yy-1,tp.id);
+        if (yy>2) itey(xx,yy-2,tp.id);
         itey(xx,yy+1,tp.id);
+        itey(xx,yy+2,tp.id);
     }
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < k; i++) {
         if (debug) {
             printf("d[%d]: %d\n", i, d[i]);
         }
@@ -89,7 +101,13 @@ int main() {
         for (int idx:vx[n]) {
             ans = min(ans, d[idx]+1);
         }
+        if (n>1) for (int idx:vx[n-1]) {
+            ans = min(ans, d[idx]+1);
+        }
         for (int idy:vy[m]) {
+            ans = min(ans, d[idy]+1);
+        }
+        if (m>1) for (int idy:vy[m-1]) {
             ans = min(ans, d[idy]+1);
         }
         if (ans == MOD) printf("-1\n");
@@ -98,3 +116,4 @@ int main() {
 
 	return 0;
 }
+
